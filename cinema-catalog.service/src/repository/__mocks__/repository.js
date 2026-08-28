1ee0204b0247f352effdb8df50b39470f32dd995
marcio8queiroz <marcio8queiroz@hotmail.com>
2026-08-28 00:10:47 -0400

    dados exemplo

--- cinema-catalog.service/src/repository/__mocks__/repository.js
+++ cinema-catalog.service/src/repository/__mocks__/repository.js
@@ -1,65 +1,185 @@
-const movies = [{
-        "_id":  "691be0990ebd7804bece5f47",
-        "titulo": "Vingadores: Guerra Infinita",
-        "sinopse": "Os heróis mais poderosos da Marvel enfrentando o Thanos",
-        "duracao": 120,
-        "dataLancamento": new Date("2025-10-20T00:00:00.000Z"),
-        "imagem": "http://www.luiztools.com.br/vingadores-gi.jpg",
-        "categorias": [
-            "Aventura",
-            "Ação"
+const cinemaCatalog = [
+  {
+    cidade: "Gravataí",
+    uf: "RS",
+    pais: "BR",
+    cinemas: []
+  },
+  {
+    cidade: "Porto Alegre",
+    uf: "RS",
+    pais: "BR",
+    cinemas: [
+      {
+        _id: ObjectId(),
+        nome: "Cinemark Bourbon Ipiranga",
+        salas: [
+          {
+            nome: 1,
+            sessoes: [
+              {
+                data: ISODate("2026-08-28T19:00:00Z"),
+                idFilme: ObjectId("691be0990ebd7804bece5f47"),
+                filme: "Vingadores: Guerra Infinita",
+                valor: 25.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: false },
+                  { numero: 3, disponivel: true },
+                  { numero: 4, disponivel: true },
+                  { numero: 5, disponivel: false }
+                ]
+              },
+              {
+                data: ISODate("2025-08-06T21:30:00Z"),
+                idFilme: ObjectId("691be514a6dda6c3b5ce5f47"),
+                filme: "Vingadores: Ultimato",
+                valor: 30.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: true },
+                  { numero: 3, disponivel: false },
+                  { numero: 4, disponivel: true },
+                  { numero: 5, disponivel: true }
+                ]
+              }
+            ]
+          },
+          {
+            nome: 2,
+            sessoes: [
+              {
+                data: ISODate("2024-03-28T20:00:00Z"),
+                idFilme: ObjectId("691be514a6dda6c3b5ce5f49"),
+                filme: "Vingadores: Era de Ultron",
+                valor: 32.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: true },
+                  { numero: 3, disponivel: true },
+                  { numero: 4, disponivel: false },
+                  { numero: 5, disponivel: false }
+                ]
+              }
+            ]
+          }
         ]
-    },
-    {
-        "_id": "691be514a6dda6c3b5ce5f47",
-        "titulo": "Os Vingadores: Ultimato",
-        "sinopse": "Os heróis mais poderosos da Terra enfrentando o Thanos. De novo.",
-        "duracao": 181,
-        "dataLancamento": new Date(),
-        "imagem": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "categorias": [
-            "Aventura",
-            "Ação"
+      },
+      {
+        _id: ObjectId(),
+        nome: "UCI New York City Center",
+        salas: [
+          {
+            nome: 1,
+            sessoes: [
+              {
+                data: ISODate("2023-09-29T18:00:00Z"),
+                idFilme: ObjectId("691be514a6dda6c3b5ce5f4a"),
+                filme: "Os Vingadores",
+                valor: 35.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: true },
+                  { numero: 3, disponivel: false },
+                  { numero: 4, disponivel: true },
+                  { numero: 5, disponivel: true }
+                ]
+              }
+            ]
+          }
         ]
-    },
-    {
-        "_id": "691be514a6dda6c3b5ce5f48",
-        "titulo": "Os Vingadores: Guerra Infinita",
-        "sinopse": "Os heróis mais poderosos da Terra enfrentando o Thanos",
-        "duracao": 149,
-        "dataLancamento": {
-            "$date": "2018-04-26T00:00:00.000Z"
-        },
-        "imagem": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "categorias": [
-            "Aventura",
-            "Ação"
+      }
+    ]
+  },
+  {
+    cidade: "Manaus",
+    uf: "AM",
+    pais: "BR",
+    cinemas: [
+      {
+        _id: ObjectId(),
+        nome: "Cinemais Millennium",
+        salas: [
+          {
+            nome: 1,
+            sessoes: [
+              {
+                data: ISODate("2022-05-30T19:30:00Z"),
+                idFilme: ObjectId("66d000000000000000000005"),
+                filme: "Batman",
+                valor: 28.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: false },
+                  { numero: 3, disponivel: true },
+                  { numero: 4, disponivel: true },
+                  { numero: 5, disponivel: true }
+                ]
+              }
+            ]
+          }
         ]
-    },
-    {
-        "_id": "691be514a6dda6c3b5ce5f49",
-        "titulo": "Os Vingadores: Era de Ultron",
-        "sinopse": "Os heróis mais poderosos da Terra enfrentando o Ultron",
-        "duracao": 141,
-        "dataLancamento": new Date("2015-04-23T00:00:00.000Z"),
-        "imagem": "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "categorias": [
-            "Aventura",
-            "Ação"
+      },
+      {
+        _id: ObjectId(),
+        nome: "Cinépolis Ponta Negra",
+        salas: [
+          {
+            nome: 1,
+            sessoes: [
+              {
+                data: ISODate("2021-08-30T21:00:00Z"),
+                idFilme: ObjectId("691be514a6dda6c3b5ce5f4b"),
+                filme: "Vingadores: Nova Era",
+                valor: 31.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: true },
+                  { numero: 3, disponivel: true },
+                  { numero: 4, disponivel: false },
+                  { numero: 5, disponivel: true }
+                ]
+              }
+            ]
+          }
         ]
-    },
-    {
-        "_id": "691be514a6dda6c3b5ce5f4a",
-        "titulo": "Os Vingadores",
-        "sinopse": "Os heróis mais poderosos da Terra enfrentando o Loki",
-        "duracao": 143,
-        "dataLancamento": new Date("2012-04-27T00:00:00.000Z"),
-        "imagem": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "categorias": [
-            "Aventura",
-            "Ação"
+      }
+    ]
+  },
+  {
+    cidade: "São Paulo",
+    uf: "SP",
+    pais: "BR",
+    cinemas: [
+      {
+        _id: ObjectId(),
+        nome: "Cinemark Eldorado",
+        salas: [
+          {
+            nome: 1,
+            sessoes: [
+              {
+                data: ISODate("1999-08-31T20:00:00Z"),
+                idFilme: ObjectId("66d000000000000000000007"),
+                filme: "Matrix",
+                valor: 34.0,
+                assentos: [
+                  { numero: 1, disponivel: true },
+                  { numero: 2, disponivel: true },
+                  { numero: 3, disponivel: false },
+                  { numero: 4, disponivel: false },
+                  { numero: 5, disponivel: true }
+                ]
+              }
+            ]
+          }
         ]
-    }];
+      }
+    ]
+  }
+];
+
+export default cinemaCatalog;
 
 async function getAllMovies() {
     return movies;
